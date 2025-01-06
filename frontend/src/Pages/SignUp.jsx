0052d1553c32c995948e3dabc89a2cc8e6375e41
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-06 15:35:46 +0500

    Update SignUp.jsx

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -185,7 +185,7 @@ const SignUp = () => {
               <button type="submit">CONTINUE</button>
             </form>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Signin here</a>
+                      Already have an account? <a href="/signin" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Signin here</a>
                   </p>
           </div>
         </div>
