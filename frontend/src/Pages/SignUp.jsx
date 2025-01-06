dbf7453919af787c1ca4a4cd82358f3f550aee01
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-06 14:27:46 +0500

    Update SignUp.jsx

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -149,7 +149,7 @@ const SignUp = () => {
         </div>
         <div className="loginDetail">
           <div>
-            <h3>Login</h3>
+            <h3>Sign Up</h3>
           </div>
 
           <div>
