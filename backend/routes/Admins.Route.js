2c5938b75c79ca5c2f0eca9f1a38040354ee95a9
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-07 10:13:29 +0500

    Update Admins.Route.js

--- backend/routes/Admins.Route.js
+++ backend/routes/Admins.Route.js
@@ -44,12 +44,12 @@ router.get("/all", async (req, res) => {
 //           });
 //           await admin.save();
 //           let newAdmin = await AdminModel.find({ email });
-
+               // let myPassword = process.env.PASS;
 //           const transporter = nodemailer.createTransport({
 //             service: "gmail",
 //             auth: {
 //               user: "ali4282271@gmail.com",
-//               pass: "vuzy kaaz xmeg fdhy",
+//               pass: myPassword,
 //             },
 //           });
 
@@ -94,12 +94,12 @@ router.post("/register", async (req, res) => {
           });
           await admin.save();
           let newAdmin = await AdminModel.find({ email });
-
+          let myPassword = process.env.PASS;
           const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
               user: "ali4282271@gmail.com",
-              pass: "vuzy kaaz xmeg fdhy",
+              pass: myPassword,
             },
           });
 
